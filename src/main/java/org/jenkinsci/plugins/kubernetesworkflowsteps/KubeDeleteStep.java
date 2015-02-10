9d8e769bd63e607bc01f4a0240821d477bc6d535
elibixby <eli.bixby@gmail.com>
2015-02-10 13:25:44 -0800

    Added licenses

--- src/main/java/org/jenkinsci/plugins/kubernetesworkflowsteps/KubeDeleteStep.java
+++ src/main/java/org/jenkinsci/plugins/kubernetesworkflowsteps/KubeDeleteStep.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package org.jenkinsci.plugins.kubernetesworkflowsteps;
 
 import org.apache.http.client.methods.HttpDelete;
@@ -13,8 +29,8 @@ public class KubeDeleteStep extends KubeStep {
   public final transient String id;
   
   /**
-   * @param resource
-   * @param id
+   * @param resource A String that is one of { "pods", "services", "replicationControllers" }
+   * @param id A String giving the id ("name") of the resource to be deleted
    */
   @DataBoundConstructor
   public KubeDeleteStep(String resource, String id) {
